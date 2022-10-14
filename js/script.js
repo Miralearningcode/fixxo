6af89c67a8260f45da4d801ddb9c1113786160e8
Mira Wänseth <mira.wanseth@utb.ecutbildning.se>
2022-10-14 15:26:13 +0200

    finished js validate for contact-form

--- js/script.js
+++ js/script.js
@@ -1,28 +1,40 @@
-const checkLength = (element, errorMessage = "This field is required", minLength = 1) => {
-    console.log(element.target.id)
+const checkLength = (element, minLength = 2, message) => {
+    
+    if(message === undefined)
+        message = `Your ${element.target.id} must contain at least ${minLength} characters`
+
     if (element.target.value.length < minLength) {
         document.getElementById(element.target.id).classList.add('error')
-        document.getElementById(`${element.target.id}ErrorMessage`).innerText = errorMessage
+        document.getElementById(`${element.target.id}ErrorMessage`).innerHTML = message
     } else {
         document.getElementById(element.target.id).classList.remove('error')
         document.getElementById(`${element.target.id}ErrorMessage`).innerText = ""
     }
 }
 
-const checkEmail = (element) => {
-    if (element.match(/))
+const checkEmail = (element, message) => {
+    if(message === undefined)
+    message = `Your ${element.target.id} must be a valid email adress`
+
+    if (!element.target.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+    document.getElementById(element.target.id).classList.add('error')
+    document.getElementById(`${element.target.id}ErrorMessage`).innerText = message
+    } else {
+        document.getElementById(element.target.id).classList.remove('error')
+        document.getElementById(`${element.target.id}ErrorMessage`).innerText = ""
+    }
 }
 
 const validate = (e) => {
     switch(e.target.type) {
             case "text":
-                //2= minimum of 2 character
-                checkLength(e, `You must enter a ${e.target.id}`, 2)
+                checkLength(e)
                 break;
             case "email":
+                checkEmail(e)
                 break;
             case "textarea":
-                checkLength(e, `You must enter a ${e.target.id}`, 5)
+                checkLength(e, 5)
                 break;
     }
 }
