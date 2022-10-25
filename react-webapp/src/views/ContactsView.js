1db55567d06b336dadacc1ccbd03419b2bf2bce3
Mira Wänseth <mira.wanseth@utb.ecutbildning.se>
2022-10-25 16:05:52 +0200

    added BreadcrumbSection

--- react-webapp/src/views/ContactsView.js
+++ react-webapp/src/views/ContactsView.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import BreadcrumbSection from '../sections/BreadcrumbSection'
 import FooterSection from '../sections/FooterSection'
 import NavigationBarSection from '../sections/NavigationBarSection'
 
 const ContactsView = () => {
+  window.top.document.title = 'Contacts | Fixxo.'
+
   return (
     <>
       <NavigationBarSection />
+      <BreadcrumbSection currentPage="Contacts" />
       <FooterSection />
     </>
   )
